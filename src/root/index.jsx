796af7e3ac8bb2117cf00fb4fdc905d845d04805
Sardor0795 <english.programmer.9595@gmail.com>
2023-09-02 11:43:38 +0500

    Fix fonts

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { SignInPage } from "../pages/signIn";
+// import Sidebar from "../components/sideBar";
 
 export const Root = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<SignInPage />} />
+        {/* <Route path="/" element={<Sidebar />} /> */}
       </Routes>
     </>
   );
