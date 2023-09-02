dddf812dd33cbaa10b766babe261e182a80ce6da
Sardor0795 <english.programmer.9595@gmail.com>
2023-09-02 15:38:50 +0500

    Add sidebar Data

---
+++ src/utils/sidebarData.jsx
@@ -0,0 +1,16 @@
+import { ClothesIcon, UsersProfileIcon } from "../assets/icon";
+
+export const sidebarData = [
+  {
+    id: 1,
+    title: "Продавцы",
+    path: "/sales",
+    icon: <UsersProfileIcon />,
+  },
+  {
+    id: 2,
+    title: "Одежда",
+    path: "/clothes",
+    icon: <ClothesIcon />,
+  },
+];
