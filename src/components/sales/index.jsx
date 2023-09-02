595730b6e84a255845bf2823e4f765c7abbc2fd7
Sardor0795 <english.programmer.9595@gmail.com>
2023-09-02 18:02:08 +0500

    Add date picker

--- src/components/sales/index.jsx
+++ src/components/sales/index.jsx
@@ -1,3 +1,33 @@
+import { SearchIcon } from "../../assets/icon";
+import { Space, DatePicker } from "antd";
+
 export const SalesComponent = () => {
-  return <div className="w-full">Sales</div>;
+  const { RangePicker } = DatePicker;
+
+  return (
+    <div className="w-full">
+      <div className="px-10">
+        <div className="border-b py-[18px] flex items-center justify-between">
+          <label className="px-[13px] w-full max-w-[400px] flex items-center border border-searchBgColor rounded-lg ">
+            <input
+              className="text-[13px] md:text-base outline-none	 w-full h-[40px] xs:h-12 placeholder-not-italic placeholder-font-AeonikProMedium  placeholder-text-black"
+              type="email"
+              placeholder="Поиск"
+              required
+              inputMode="search"
+            />
+            <span className="cursor-pointer">
+              <SearchIcon />
+            </span>
+          </label>
+
+          <section className="mobileDate hidden md:flex items-center">
+            <Space direction="vertical" size={12}>
+              <RangePicker style={{ fontSize: "16px", padding: "13px" }} />
+            </Space>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
