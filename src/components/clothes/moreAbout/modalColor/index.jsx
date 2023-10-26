76fe44f52b5673257f4f9deecf63cbd11922c0f0
Sardor0795 <english.programmer.9595@gmail.com>
2023-10-26 19:21:36 +0500

    Finish colors modal

--- src/components/clothes/moreAbout/modalColor/index.jsx
+++ src/components/clothes/moreAbout/modalColor/index.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { ColoredCardsIcon, StarIcon, XIcon } from "../../../../assets/icon";
+import {
+  ColoredCardsIcon,
+  PlusIcon,
+  StarIcon,
+  XIcon,
+} from "../../../../assets/icon";
 
 export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
   return (
@@ -25,7 +30,10 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
               Фильт цвет
             </div>
           </div>
-          <button className="p-[5px] border border-[F2F2F2] rounded-lg">
+          <button
+            onClick={() => setColorModalOpen(false)}
+            className="p-[5px] border border-[F2F2F2] rounded-lg"
+          >
             <XIcon />
           </button>
         </div>
@@ -37,7 +45,202 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
           <div className="w-5 h-5 rounded-full bg-black"></div>
         </div>
 
-        <div className="border border-[#F2F2F2] rounded-lg p-4"></div>
+        <div className="border border-[#F2F2F2] rounded-lg p-4">
+          <div className="flex justify-between mb-[25px]">
+            <div>
+              <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                Размер Груди
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  2
+                </div>
+                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  5
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                <div className="mr-[5px]">Размер</div> <StarIcon />
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  21
+                </div>
+                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="bg-[#F8FCFF] h-[38px] w-[60px] flex items-center justify-center border border-[#E5E5E5] rounded-lg">
+                  <PlusIcon />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                Буквенный Размер
+              </div>
+              <div className="">
+                <div className="flex items-center mb-[10px]">
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      XXS
+                    </div>
+                  </div>
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      XS
+                    </div>
+                  </div>
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      S
+                    </div>
+                  </div>
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      M
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex items-center">
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      L
+                    </div>
+                  </div>
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      XL
+                    </div>
+                  </div>
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      2XL
+                    </div>
+                  </div>
+                  <div className="flex items-center cursor-pointer mr-[10px]">
+                    <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                    <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                      3XL
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between mb-[25px]">
+            <div>
+              <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                Размер Талии
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  5
+                </div>
+                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  10
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                Размер Бедер
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  15
+                </div>
+                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  21
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                <div className="mr-[5px]">Количество</div> <StarIcon />
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  6
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between mb-[25px]">
+            <div>
+              <div className="font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
+                Возраст
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                  40
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
+                <div className="mr-[5px]">Цена</div> <StarIcon />
+              </div>
+              <div className="flex items-center">
+                <div className="flex items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                  10 000 000{" "}
+                  <div className="ml-[8px] text-[14px] font-AeonikProRegular text-[#8C8C8C]">
+                    сум
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
+                Скидка{" "}
+                <div className="text-[#8C8C8C] font-AeonikProMedium text-[13px] ml-[10px]">
+                  (не обезательно)
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="flex items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                  13 <div className="text-[#8C8C8C] ml-[8px]">%</div>
+                </div>
+                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="flex items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                  10 000 000{" "}
+                  <div className="ml-[8px] text-[14px] font-AeonikProRegular text-[#8C8C8C]">
+                    сум
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center">
+              <div className="font-AeonikProRegular text-[16px] mr-[10px]">
+                Цвет:
+              </div>
+              <div className="bg-[#FFB029] text-[14px] font-AeonikProMedium py-[3px] px-[12px] rounded-[15px] text-white">
+                Желтый
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
