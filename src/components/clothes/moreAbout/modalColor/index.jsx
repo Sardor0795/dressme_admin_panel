c9fccdbb7aed6810e3d8bac05ea4658590e907d0
Sardor0795 <english.programmer.9595@gmail.com>
2023-11-02 16:07:13 +0500

    Make all sizes' modal mobile version

--- src/components/clothes/moreAbout/modalColor/index.jsx
+++ src/components/clothes/moreAbout/modalColor/index.jsx
@@ -23,12 +23,15 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
             : "md:hidden bottom-[-800px] z-[-10]"
         }`}
       >
-        <div className="flex justify-between mb-[10px]">
-          <div className="border border-[F0F0F0] p-[5px] rounded-lg flex items-center">
+        <div className="flex items-center justify-between mb-[25px] md:mb-[10px]">
+          <div className="hidden border border-[F0F0F0] p-[5px] rounded-lg md:flex items-center">
             <ColoredCardsIcon />
             <div className="font-AeonikProRegular text-[14px] ml-[5px]">
               Фильт цвет
             </div>
+          </div>
+          <div className="block md:hidden text-[20px] font-AeonikProMedium text-[#303030]">
+            Все размеры
           </div>
           <button
             onClick={() => setColorModalOpen(false)}
@@ -38,7 +41,7 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
           </button>
         </div>
 
-        <div className="flex items-center mb-5">
+        <div className="hidden md:flex items-center mb-5">
           <div className="mr-2 font-AeonikProRegular text-[14px]">
             Выбранный цвет:
           </div>
@@ -46,8 +49,17 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
         </div>
 
         <div className="border border-[#F2F2F2] rounded-lg p-4">
-          <div className="flex justify-between mb-[25px]">
-            <div>
+          <div className="flex md:hidden items-center mb-[16px]">
+            <div className="text-[14px] font-AeonikProRegular text-[#303030] mr-[10px]">
+              Цвет:
+            </div>
+            <div className="text-[14px] text-white font-AeonikProMedium py-[3px] px-[15px] bg-black rounded-full">
+              Черный
+            </div>
+          </div>
+
+          <div className="flex justify-between mb-[25px] gap-[30px] md:gap-[0]">
+            <div className="w-ful md:w-fit">
               <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
                 Размер Груди
               </div>
@@ -62,7 +74,7 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
               </div>
             </div>
 
-            <div>
+            <div className="w-ful md:w-fit">
               <div className="flex items-center font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
                 <div className="mr-[5px]">Размер</div> <StarIcon />
               </div>
@@ -77,7 +89,7 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
               </div>
             </div>
 
-            <div>
+            <div className="hidden md:block">
               <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
                 Буквенный Размер
               </div>
@@ -139,8 +151,8 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
             </div>
           </div>
 
-          <div className="flex justify-between mb-[25px]">
-            <div>
+          <div className="flex justify-between mb-[25px] gap-[30px] md:gap-[0]">
+            <div className="w-full md:w-fit">
               <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
                 Размер Талии
               </div>
@@ -155,22 +167,22 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
               </div>
             </div>
 
-            <div>
+            <div className="w-full md:w-fit">
               <div className="font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
                 Размер Бедер
               </div>
               <div className="flex items-center">
-                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                <div className="w-full md:w-fit py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
                   15
                 </div>
-                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
-                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                <div className="min-w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="w-full md:w-fit py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
                   21
                 </div>
               </div>
             </div>
 
-            <div>
+            <div className="hidden md:block">
               <div className="flex items-center font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
                 <div className="mr-[5px]">Количество</div> <StarIcon />
               </div>
@@ -182,8 +194,8 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
             </div>
           </div>
 
-          <div className="flex justify-between mb-[25px]">
-            <div>
+          <div className="flex justify-between mb-[25px] gap-[30px] md:gap-[0]">
+            <div className="">
               <div className="font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
                 Возраст
               </div>
@@ -194,12 +206,12 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
               </div>
             </div>
 
-            <div>
+            <div className="w-full md:w-fit">
               <div className="flex items-center font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
                 <div className="mr-[5px]">Цена</div> <StarIcon />
               </div>
               <div className="flex items-center">
-                <div className="flex items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                <div className="flex w-full md:w-fit items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
                   10 000 000{" "}
                   <div className="ml-[8px] text-[14px] font-AeonikProRegular text-[#8C8C8C]">
                     сум
@@ -208,7 +220,7 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
               </div>
             </div>
 
-            <div>
+            <div className="w-full md:w-fit hidden md:block">
               <div className="flex items-center font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
                 Скидка{" "}
                 <div className="text-[#8C8C8C] font-AeonikProMedium text-[13px] ml-[10px]">
@@ -230,7 +242,97 @@ export default function ColorModal({ setColorModalOpen, colorModalOpen }) {
             </div>
           </div>
 
+          <div className="md:hidden block mb-[25px]">
+            <div className="font-AeonikProMedium text-[16px] text-[#303030]">
+              Буквенный Размер
+            </div>
+            <div className="flex flex-wrap">
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  XXS
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  XS
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  S
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  M
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  L
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  XL
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  2XL
+                </div>
+              </div>
+              <div className="mt-[12px] flex items-center cursor-pointer mr-[10px]">
+                <div className="w-4 h-4 border border-[#B5B5B5] rounded-sm mr-[5px]"></div>
+                <div className="font-AeonikProMedium text-[#A1A1A1] text-[14px]">
+                  3XL
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div>
+            <div className="md:hidden block mb-[25px]">
+              <div className="flex items-center font-AeonikProMedium text-[16px] mb-[10px] text-[#303030]">
+                <div className="mr-[5px]">Количество</div> <StarIcon />
+              </div>
+              <div className="flex items-center">
+                <div className="py-[10px] px-[20px] font-AeonikProMedium text-[16px] border border-[#E5E5E5] rounded-lg">
+                  6
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-fit md:hidden block">
+              <div className="flex items-center font-AeonikProRegular text-[16px] mb-[10px] text-[#303030]">
+                Скидка{" "}
+                <div className="text-[#8C8C8C] font-AeonikProMedium text-[13px] ml-[10px]">
+                  (не обезательно)
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="flex items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                  13 <div className="text-[#8C8C8C] ml-[8px]">%</div>
+                </div>
+                <div className="w-[15px] border-t border-[#E5E5E5] mx-[5px]"></div>
+                <div className="flex items-center py-[10px] px-[20px] font-AeonikProRegular text-[16px] border border-[#E5E5E5] rounded-lg">
+                  10 000 000{" "}
+                  <div className="ml-[8px] text-[14px] font-AeonikProRegular text-[#8C8C8C]">
+                    сум
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden md:block">
             <div className="flex items-center">
               <div className="font-AeonikProRegular text-[16px] mr-[10px]">
                 Цвет:
