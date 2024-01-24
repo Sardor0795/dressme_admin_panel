7da8015b2df8cb08dec5763003a9552cf51f391d
main <ismoilergashovich@gmail.com>
2024-01-24 14:10:39 +0500

    new changes

--- src/components/shops/index.jsx
+++ src/components/shops/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const ShopsComponent = () => {
-  return <div>ShopsComponent</div>;
+  return <div>ShopsComponent
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint doloribus iste libero laborum asperiores ut, dicta odio maxime eaque nostrum nam accusamus quia perspiciatis eos esse delectus nisi, laudantium tempora minima doloremque voluptatem maiores quae error blanditiis. Magnam quisquam, rem fugiat eius reiciendis reprehenderit commodi ratione quis amet ipsum explicabo.
+      
+    </p>
+  </div>;
 };
